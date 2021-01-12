735912f4fcf1fb4db557fee0081e20445d01a1da
lostways <andrew@lostways.com>
2021-01-12 12:47:02 +0000

    fixed display

--- main.py
+++ main.py
@@ -11,25 +11,52 @@ import curses
 import argparse
 import time
 import datetime as dt
-import pyfiglet
 
-def print_screen(screen,text,center=True):
+def get_time_display(time_text):
+  a = "####" 
+  b = "#  #"
+  c = "#   "
+  d = "   #"
+  o = "   "
+  x = " # "
+
+    
+               # 0,1,2,3,4,5,6,7,8,9,:
+  templates = [ [a,d,a,a,b,a,a,a,a,a,o],
+                [b,d,d,d,b,c,c,d,b,b,x],
+                [b,d,a,a,a,a,a,d,a,a,o],
+                [b,d,c,d,d,d,b,d,b,d,x],
+                [a,d,a,a,d,a,a,d,a,a,o] ]
+
+  out = ""
+  for row in templates: 
+    for digit in time_text:
+      if digit == ":":
+        digit = 10
+      else:
+        digit = int(digit,10)
+      out = out + row[digit] + " "
+    out = out + "\n"
+
+  return out
+  
+def print_screen(screen,text):
   x = 0
   y = 0
 
-  if center == True :
-    num_rows, num_cols = screen.getmaxyx()
-    middle_row = int(num_rows / 2)
-    middle_col = int(num_cols / 2)
+  num_rows, num_cols = screen.getmaxyx()
+  middle_row = int(num_rows / 2)
+  middle_col = int(num_cols / 2)
+ 
 
-    lines = text.rstrip("\n").split("\n")
-    longest_line = max(map(len,lines))
+  lines = text.rstrip("\n").split("\n")
+  longest_line = max(map(len,lines))
 
-    y = middle_row - int(len(lines) / 2)
-    x = middle_col - int(longest_line / 2)
+  y = middle_row - int(len(lines) / 2)
+  x = middle_col - int(longest_line / 2)
 
   for line in lines:
-    screen.addstr(y,x,(" " * 10) + line + (" " * 10) + "\n", curses.color_pair(1))
+    screen.addstr(y,x,line + "\n", curses.color_pair(1))
     y = y + 1
   screen.refresh()
 
@@ -98,9 +125,9 @@ def main(screen=None):
     #print(args)
     curses.curs_set(0)
     curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    pomo = Pomodoro(1,2)
+    pomo = Pomodoro(.5,1)
     pomo.start()
     screen.clear()
     while True:
@@ -112,11 +139,15 @@ def main(screen=None):
       pom_start_time = pomo.start_time
       if pom_state == "pomo":
         time_left = pom_end_time - now
+      elif pom_state == "done":
+        time_left = "00:00:00"
       else :
         time_left = break_end_time - now
       time_left = str(time_left).split(".")[0]
-      time_left_text = pyfiglet.figlet_format(f'{time_left}',font="block")
-      print_screen(screen,time_left_text)
+      time_left = "{:0>8}".format(time_left)
+      display_text = get_time_display(time_left)
+      display_text = display_text + "\n" + pom_state
+      print_screen(screen,display_text)
       time.sleep(1)
 
 if __name__ == "__main__": main()

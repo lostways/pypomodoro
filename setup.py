18215ed8bb3b2e79c8215475cac6a5aa60fe4191
lostways <andrew@lostways.com>
2021-01-18 05:31:13 +0000

    removed search for packages

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages = find_packages(),
     entry_points = {
       'console_scripts': ['pypomodoro=pypomodoro.main:main']
     },

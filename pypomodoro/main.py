f04d61b7c1f24c1abda90c4a682b8286cffc61d1
lostways <andrew@lostways.com>
2022-05-22 19:43:57 -0700

    added beeps

--- pypomodoro/main.py
+++ pypomodoro/main.py
@@ -18,7 +18,6 @@ from gui import Gui
 def init_args():
   """ This is executed when run from the command line """
   parser = argparse.ArgumentParser()
-
   parser.add_argument("-w", "--work", default=25, type=float, action="store", dest="work_mins", help="Number of miinutes for work")
   parser.add_argument("-b", "--break", default=5, type=float, action="store", dest="break_mins", help="Number of minutes for break")
   parser.add_argument("-v", "--verbose", action="store_true", help="Move detailed display")
@@ -33,12 +32,24 @@ def init_args():
   return args
 
 def start(screen):
-    state_text = {
-      'pomo' : 'Time to work',
-      'break': 'Take a break',
-      'done': 'Press space to get back to work',
-      'init': 'Press space to start work'
+    state_params = {
+      'pomo' : {
+        'text': 'Time to work',
+        'alerted': False
+      },
+      'break': {
+        'text': 'Take a break',
+        'alerted': False
+      },
+      'done': {
+        'text': 'Press space to get back to work',
+        'alerted': False
+      },
+      'init': {
+        'text': 'Press space to start work',
+      }
     }
+
     time_left_init = "00:00:00"
 
     args = init_args()
@@ -71,12 +82,14 @@ def start(screen):
       elif pom_state == "done":
         time_left = time_left_init
         if key_pressed == ' ':
+          for st in state_params:
+            state_params[st]['alerted'] = False
           pomo.restart()
           continue
       else :
         time_left = break_end_time - now
 
-      display_info = state_text[pom_state]
+      display_info = state_params[pom_state]['text']
 
       if key_pressed != '':
         display_info = "Press 'q' to quit"
@@ -89,6 +102,10 @@ def start(screen):
       time_left = "{:0>8}".format(time_left)
       display_text = gui.get_time_display(time_left)
       display_text = display_text + "\n" + display_info
+
+      if pom_state != 'init' and state_params[pom_state]['alerted'] == False:
+        gui.beep()
+        state_params[pom_state]['alerted'] = True
       gui.print_screen(display_text)
       time.sleep(1)
 
